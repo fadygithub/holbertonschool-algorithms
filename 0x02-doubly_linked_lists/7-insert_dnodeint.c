a71b29d40083f7f516e37f838e9500a25277f255
Fady Hanna <fadyhanna1@outlook.com>
2021-09-07 10:05:37 +0300

    Ädd tasks 6 to 8

---
+++ 0x02-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+#include "lists.h"
+
+/**
+ * len - calculate double linked list len.
+ * @h:  pointer to head.
+ *
+ * Return: returns the number of elements in a linked dlistint_t list.
+ */
+size_t len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: pointer to the pointer that points to head.
+ * @idx:the index of the list
+ * @n: value insert.
+ *
+ * Return:  the address of the new node, or NULL if it failed.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h;
+	dlistint_t *new;
+	unsigned int c = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (idx == 0 || *h == NULL)
+		return (add_dnodeint(h, n));
+	if (idx == len(*h))
+		return (add_dnodeint_end(h, n));
+	if (idx > len(*h))
+		return (NULL);
+	while (tmp)
+	{
+		if (c == (idx - 1))
+		{
+			new->next = tmp->next;
+			tmp->next->prev = new;
+			tmp->next = new;
+			new->prev = tmp;
+		}
+		c++;
+		tmp = tmp->next;
+	}
+	return (new);
+}
+
---
+++ 0x02-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+#include "lists.h"
+
+/**
+ * len - calculate double linked list len.
+ * @h:  pointer to head.
+ *
+ * Return: returns the number of elements in a linked dlistint_t list.
+ */
+size_t len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index.
+ * @head: pointer to the pointer that points to head.
+ * @index: the index of the node that should be deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+	dlistint_t *hold = NULL;
+	unsigned int count = 0;
+
+	if (!*head || index >= len(*head))
+		return (-1);
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		if (len(*head) >= 1)
+			(*head)->prev = NULL;
+		return (1);
+	}
+	while (tmp)
+	{
+		if (count == index - 1)
+		{
+			if (!tmp->next->next)
+			{
+				free(tmp->next);
+				tmp->next = NULL;
+				return (1);
+			}
+			hold = tmp->next->next;
+			free(tmp->next);
+			tmp->next = hold;
+			hold->prev = tmp;
+			return (1);
+		}
+		count++;
+		tmp = tmp->next;
+	}
+	return (-1);
+}
+
